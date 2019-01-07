e48820f9588f01e032a4f2fded98b2a14ab662bb
whatmode_bots <waldi59@ukr.net>
2019-01-07 12:31:25 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,31 @@
+import telebot
+import time
+
+bot_token = '626206878:AAHfV5gDgF_1P9XHWrHv2gM4xElhaBYddq8'
+
+bot = telebot.TeleBot(token=bot_token)
+
+def find_at(msg):
+    for text in msg:
+        if '@' in text:
+            return text
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, 'Welcome!')
+
+@bot.message_handler(commands=['help'])
+def send_welcome(message):
+    bot.reply_to(message, 'To use this bot, send it a username')
+
+@bot.message_handler(func=lambda msg: msg.text is not None and '@' in msg.text)
+def at_answer(message):
+    texts = message.text.split()
+    at_text = find_at(texts)
+
+    bot.reply_to(message, 'https://instagram.com/{}'.format(at_text[1:]))
+while True:
+    try:
+        bot.polling()
+    except Exception:
+        time.sleep(15)
